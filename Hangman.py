ef95e7c298fc8517bc16f42289afd6eea79e0fda
cjsnyder <cjsnyder1018@gmail.com>
2017-07-07 17:06:33 -0500

    Creates blanks, takes input and fills blanks.

--- Hangman.py
+++ Hangman.py
@@ -4,24 +4,30 @@ Use: Learning and test of knowledge"""
 
 from random import randint
 
-def initalizeBlanks():
-    states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
-              'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho',
-              'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-              'Maine' 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-              'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada',
-              'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
-              'North Carolina', 'North Dakota', 'Ohio',
-              'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-              'South  Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah',
-              'Vermont', 'Virginia', 'Washington', 'West Virginia',
-              'Wisconsin', 'Wyoming']
-    choice = randint(0, 50)
-    compWord = states[choice]
-    blanks = []
-    for letters in compWord:
-        blanks += "_"
-    return blanks
 
+states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
+          'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho',
+          'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+          'Maine' 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+          'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada',
+          'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
+          'North Carolina', 'North Dakota', 'Ohio',
+          'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+          'South  Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah',
+          'Vermont', 'Virginia', 'Washington', 'West Virginia',
+          'Wisconsin', 'Wyoming']
+choice = randint(0, 50)
+compWord = states[choice]
+blanks = []
+for letters in compWord:
+    blanks += "_"
 
-print(initalizeBlanks())
+def checkGuess(userInput):
+    if userInput.isalpha():
+        if userInput in compWord:
+            blanks[compWord.index(userInput)] = userInput
+
+while blanks != compWord:
+    userInput = input("Input you letter: ")
+    checkGuess(userInput)
+    print(blanks)
